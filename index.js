f92bded718e20d270dab857dc86ec661bb3a0e3d
NoahConner <noahconner1512@gmail.com>
2023-09-13 12:33:27 +0500

    parsing request path and trimmed the path

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // serve
-var server = http.createServer(function(res,res){
+var server = http.createServer(function(req,res){
+
+    // get the url and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // send the res
     res.end('Hello\n');
+
+    // log the path
+    console.log('Request recieved on path: '+ trimmedPath)
+    
 });
 
 // start server on port 3000
